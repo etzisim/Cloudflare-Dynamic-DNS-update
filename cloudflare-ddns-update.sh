a745be6a90492c0695c19faea2e58b985725f35e
etzisim <admin@etzi.at>
2022-01-24 14:07:36 +0100

    Update cloudflare-ddns-update.sh

--- cloudflare-ddns-update.sh
+++ cloudflare-ddns-update.sh
@@ -3,7 +3,6 @@
 # A bash script to update a Cloudflare DNS A record with the external IP of the source machine
 # Used to provide DDNS service for my home
 # Needs the DNS record pre-creating on Cloudflare
-§
 # Proxy - uncomment and provide details if using a proxy
 #export https_proxy=http://<proxyuser>:<proxypassword>@<proxyip>:<proxyport>
 
@@ -14,29 +13,41 @@ dnsrecord=SUB.DOMAIN.COM
 
 ## Cloudflare authentication details
 ## keep these private
-cloudflare_auth_email=YOUR@mail.com
-cloudflare_auth_key=PAI-Token
-
+cloudflare_auth_email=YOUR@MAIL.com
+api_key=YOUR-API-Token
 
 # Get the current external IP address
 ip=$(curl -s -X GET https://checkip.amazonaws.com)
 
 echo "Current IP is $ip"
 
-zoneid=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
-     -H "Authorization: Bearer $cloudflare_auth_key" \
+zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+     -H "Authorization: Bearer $api_key" \
      -H "Content-Type:application/json" | jq -r '{"result"}[] | .[0] | .id')
 
 echo "Zoneid for $zone is $zoneid"
 
-dnsrecordid=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
-     -H "Authorization: Bearer $cloudflare_auth_key" \
+dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+     -H "Authorization: Bearer $api_key" \
      -H "Content-Type:application/json" | jq -r '{"result"}[] | .[0] | .id')
 
 echo "DNSrecordid for $dnsrecord is $dnsrecordid"
 
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
-     -H "Authorization: Bearer $cloudflare_auth_key" \
+ip_cloudflare=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+     -H "Authorization: Bearer $api_key" \
+     -H "Content-Type:application/json" | jq -r '{"result"}[] | .[0] | .content')
+
+if [ $ip == $ip_cloudflare ]; then
+  echo "IP is already up to date IP: $ip"
+  exit 0
+else
+  echo "Actual IP on Cloudflare for $dnsrecord is $ip_cloudflare new ip is $ip"
+fi
+
+# update DNS record
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
+     -H "Authorization: Bearer $api_key" \
      -H "Content-Type:application/json" \
   --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false}" | jq
-  
+
+exit 0
